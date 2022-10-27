544f3831c270e320eded89a0db261b2fe54766df
Your Name <felix.acevedo.tech@gmail.com>
2022-10-26 22:05:01 -0400

    working on click logic

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 'use strict';
 
-let book1 = document.getElementsByClassName('.book-1');
-let book2 = document.getElementsByClassName('.book-2');
-let book3 = document.getElementsByClassName('.book-3');
-let book4 = document.getElementsByClassName('.book-4');
+let book1 = document.querySelector('.book-1');
+let book2 = document.querySelector('.book-2');
+let book3 = document.querySelector('.book-3');
+let book4 = document.querySelector('.book-4');
+let voteBook = document.querySelector('article');
+let bookNum = [1, 2, 3, 4];
 
-document.querySelector('.book').addEventListener('click', function () {
-  let vote = document.querySelector('article');
+voteBook.addEventListener('click', function () {
+  let vote = 'vote 1';
   console.log(vote);
 
-  if (vote) {
+  if (`book-${bookNum}` === 1) {
     console.log('Voted for Book 1!');
-  } else if (vote === book2) {
+  } else if (`book-${bookNum}` === 2) {
     console.log('Voted for Book 2!');
   } else if (vote === book3) {
     console.log('Voted for Book 3!');
