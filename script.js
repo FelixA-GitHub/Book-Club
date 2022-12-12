594028c3ae514f034916101e8de004f76b3bd9de
Your Name <felix.acevedo.tech@gmail.com>
2022-12-11 19:46:51 -0500

    decided to do modal for preview btn

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+//elements
+const btnPreview = document.querySelector('.preview');
+const btnShelf = document.querySelector('.shelf');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
